6100a788bce4153c5c2cf0bcd25f636e863796e4
Ryan Shan <ryanshan28@gmail.com>
2019-07-25 11:53:15 -0700

    initial commit

---
+++ src/rpc/SearchItem.java
@@ -0,0 +1,48 @@
+package rpc;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+
+import entity.Item;
+import external.TicketMasterClient;
+
+/**
+ * Servlet implementation class SearchItem
+ */
+@WebServlet("/search")
+public class SearchItem extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SearchItem() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		double lat = Double.parseDouble(request.getParameter("lat"));
+		double lon = Double.parseDouble(request.getParameter("lon"));
+		TicketMasterClient client = new TicketMasterClient();
+		List<Item> events = client.search(lat, lon, "event");
+		JSONArray JArray = new JSONArray();
+		for (Item i : events) {
+			JArray.put(i.toJSONObject());
+		}
+		 
+		RpcHelper.writeJsonArray(response, JArray);
+	}
+
+}
