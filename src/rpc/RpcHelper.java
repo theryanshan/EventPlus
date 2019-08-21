d4acbeeb4b1db5b5238bd4982a98f84e34d9f17e
Ryan Shan <ryanshan28@gmail.com>
2019-08-20 17:33:58 -0700

    Added getJSONArray helper method

--- src/rpc/RpcHelper.java
+++ src/rpc/RpcHelper.java
@@ -3,12 +3,15 @@ package rpc;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+
+import entity.Item;
 
 public class RpcHelper {
 	// Writes a JSONArray to http response.
@@ -45,5 +48,18 @@ public class RpcHelper {
 
 		return new JSONObject();
 	}
+	
+	// Converts a list of Item objects to JSONArray.
+	  public static JSONArray getJSONArray(List<Item> items) {
+	    JSONArray result = new JSONArray();
+	    try {
+	      for (Item item : items) {
+	        result.put(item.toJSONObject());
+	      }
+	    } catch (Exception e) {
+	      e.printStackTrace();
+	    }
+	    return result;
+	  }
 
 }
